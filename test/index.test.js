b3caecd20d5a413ec599dffb3c0d990170d4d842
Meilky <mikaeljean2k3@gmail.com>
2020-09-11 22:41:40 -0400

    Fix test for last commit

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,9 @@ test("Run without the setup sequence", () => {
 		authsys.auth();
 	} catch (error) {
 		expect(error).toBeInstanceOf(Error);
-		expect(error).toHaveProperty("message", "Something bad happened!");
+		expect(error).toHaveProperty(
+			"message",
+			"The process.env.TokenKey isn't supose to be null, got to https://github.com/Meilky/simple-authsys/blob/master/README.md to resolve this error",
+		);
 	}
 });
